c3bdab0d884260f2ec5c71496cc0fcc667fdb2ee
Luci1274 <luciano.gonzalez@alumnos.ispi4038.edu.ar>
2024-06-05 16:25:07 -0300

    intento 3

--- logica/tp3_secuencia/secuencia.py
+++ logica/tp3_secuencia/secuencia.py
@@ -19,13 +19,16 @@ def mostrar_secuencia(s):
         print(interador)
     return
 
+def crear_pagina():
+    try:
+        archivo = open("logica/tp3_secuencia/secuencia.txt", "x")
+        archivo.close
+    except:
+        pass
 
+    
 """cuerpo principal del programa"""
-try:
-    archivo = open("logica/tp3_secuencia/secuencia.txt", "x")
-    archivo.close
-except:
-    pass
+crear = crear_pagina
 
 valor_inicial = ingresar_dato("ingrese un valor numerico inicial: ")
 valor_final = ingresar_dato("ingrese un valor numerico final: ")
@@ -34,9 +37,11 @@ salto = ingresar_dato("ingrese el salto: ")
 secuencia = range(valor_inicial, valor_final, salto)
 mostrar_secuencia(secuencia)
 
+
+
 archivo = open("logica/tp3_secuencia/secuencia.txt", "a")
 archivo.write("\n")
 for valor in secuencia:
     archivo.write(str(valor))
-    clearConsole
+archivo.write("\n")
 archivo.close()
